01634dc762aa4a046fa334b4fc99da3bca97808d
yunweneric <yunweneric@gmail.com>
2024-06-12 07:53:07 +0100

    Added assets

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,5 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_open_animate/pages/components/nar_bar.dart';
-import 'package:flutter_open_animate/pages/components/shaded_bottle.dart';
 import 'package:flutter_open_animate/utils/colors.dart';
 import 'package:flutter_open_animate/utils/sizing.dart';
 import 'package:flutter_svg/svg.dart';
@@ -57,8 +55,6 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
       backgroundColor: AppColors.bgBlack,
       body: Stack(
         children: [
-          // Image.asset("assets/images/dark_bottle.png"),
-
           Positioned(
             child: Center(
               child: Image.asset(
@@ -67,14 +63,6 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
               ),
             ),
           ),
-
-          // Positioned(
-          //   child: Transform.translate(
-          //     offset: Offset(-translate, 0),
-          //     // child: Image.asset("assets/images/dark_pattern.png"),
-          //     child: SvgPicture.asset("assets/images/clipper.svg"),
-          //   ),
-          // ),
           PageView.builder(
             itemCount: 2,
             controller: pageController,
@@ -109,59 +97,6 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
               fit: BoxFit.fitHeight,
             ),
           ),
-
-          // PageView.builder(
-          //   itemCount: 2,
-          //   controller: bottleController,
-          //   itemBuilder: (c, i) {
-          //     return AnimatedContainer(
-          //       duration: Duration(milliseconds: 1000),
-          //       height: Sizing.height(context),
-          //       width: Sizing.width(context),
-          //       color: Colors.teal.withOpacity(0.1),
-          //       // child: Center(
-          //       //   child: Image.asset(
-          //       //     activeIndex == 0 ? "assets/images/light_pattern.png" : "assets/images/dark_pattern.png",
-          //       //     fit: BoxFit.fitHeight,
-          //       //   ),
-          //       // ),
-          //     );
-          //   },
-          // ),
-
-          PageView.builder(
-            itemCount: 2,
-            scrollDirection: Axis.vertical,
-            controller: leavePageController,
-            itemBuilder: (c, i) {
-              return Transform.translate(
-                offset: Offset(0, Sizing.height(context) * 0.1),
-                child: Container(
-                  child: ImageFiltered(
-                    imageFilter: ImageFilter.blur(sigmaX: 0.0, sigmaY: 2.5),
-                    child: SvgPicture.asset(i == 1 ? "assets/images/dark_leaves.svg" : "assets/images/light_leaves.svg"),
-                  ),
-                ),
-              );
-            },
-          ),
-
-          TweenAnimationBuilder(
-            key: ValueKey(activeIndex),
-            tween: Tween<Offset>(begin: Offset(50, 0), end: Offset(0, 0)),
-            duration: duration,
-            builder: (context, value, child) {
-              return Transform.translate(
-                offset: value,
-                // child: Center(
-                //   child: Image.asset("assets/images/dark_pattern.png"),
-                // ),
-              );
-            },
-          ),
-
-          const ShadedBottle(),
-
           NavBar(activeIndex: activeIndex, onToggle: animate),
         ],
       ),
